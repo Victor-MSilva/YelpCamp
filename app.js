022c4cfd2c8cf0af4a6ac31a899de484794e8e6b
VictorMarques <victormarquesilva@gmail.com>
2020-07-13 08:35:11 -0300

    final basics

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const app = express();
+const rp = require('request-promise');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let campgrounds = [
+    {name: "Salmon Creek", image: "https://www.oregonlive.com/resizer/0FVk7bpZHdb--Lw10Y-443t0ylM=/450x0/smart/arc-anglerfish-arc2-prod-advancelocal.s3.amazonaws.com/public/DUNWFNGOAVCRLAHO4ZPTBKNJEM.jpg"},
+    {name: "Granite Hill", image: "https://www.oregonlive.com/resizer/0FVk7bpZHdb--Lw10Y-443t0ylM=/450x0/smart/arc-anglerfish-arc2-prod-advancelocal.s3.amazonaws.com/public/DUNWFNGOAVCRLAHO4ZPTBKNJEM.jpg"},
+    {name: "Mountain Goat", image: "https://www.oregonlive.com/resizer/0FVk7bpZHdb--Lw10Y-443t0ylM=/450x0/smart/arc-anglerfish-arc2-prod-advancelocal.s3.amazonaws.com/public/DUNWFNGOAVCRLAHO4ZPTBKNJEM.jpg"}
+]
+
+
+// rp('https://jsonplaceholder.typicode.com/users/1')
+// .then((body) => {
+//     const parsedData = JSON.parse(body);
+//     console.log(parsedData.name + ' lives in ' + parsedData.address.street);
+// })
+// .catch((err) => {
+//     console.log('Error', err);
+// });
+
 
 app.set("view engine", "ejs");
 
@@ -8,15 +28,24 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://www.nps.gov/lavo/planyourvisit/images/southwest-campground_6081614_2.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-        {name: "Greek scret", image: "https://www.nps.gov/lavo/planyourvisit/images/southwest-campground_6081614_2.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-        {name: "Mountain Goat's Rest", image: "https://www.nps.gov/lavo/planyourvisit/images/southwest-campground_6081614_2.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-    ]
     res.render("campgrounds", {campgrounds:campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, function(){
-    console.log("Server has started!!!");
+    console.log("YelpCamp server has started!!!");
 });
